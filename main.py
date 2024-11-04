12bfab8fb09f1a6926bad8f58288756773cdd25f
aidarminyazev <legodark@mail.ru>
2024-11-05 00:26:26 +0500

    changed cache key builder

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from redis import asyncio as aioredis
 from api.routers.trade import router
 from config import REDIS_HOST, REDIS_PORT
 
+def url_key_builder(func, *args, **kwargs):
+    request = kwargs["request"]
+    return str(request.url)
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     redis_client = aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}")
-    FastAPICache.init(RedisBackend(redis_client), prefix="fastapi-cache")
+    FastAPICache.init(RedisBackend(redis_client), prefix="fastapi-cache", key_builder=url_key_builder)
     yield
 
 
